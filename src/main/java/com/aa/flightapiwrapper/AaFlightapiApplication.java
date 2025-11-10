eba23d4c83e0d38a5177dce5137ee91104a3aefa
Prateek Jain <prateek.jain@peerislands.io>
2025-11-10 09:19:04 +0530

    Add scaffolded project: aa-flightapi

---
+++ src/main/java/com/aa/flightapiwrapper/AaFlightapiApplication.java
@@ -0,0 +1,13 @@
+package com.aa.flightapiwrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AaFlightapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AaFlightapiApplication.class, args);
+    }
+
+}
